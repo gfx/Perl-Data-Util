11edcace9d48eca8df95ddbdda166bdc11e3bb19
gfx <gfuji@cpan.org>
2009-10-21 14:28:35 +0900

    Checking in changes prior to tagging of version 0.54.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 00cc2ef..57f6827 100755
    --- a/Changes
    +++ b/Changes
    @@ -1,7 +1,11 @@
     Revision history for Data-Util
    
    +0.54 Wed Oct 21 14:24:29 2009
    +    - re-fix the method-modifiers issue (thanks @nekoya)
    +        - this issue seems related to RT #69939
    +
     0.53 Mon Oct 19 19:08:19 2009
    -    - fix an issue which broke method modifiers in some cases
    +    - fix an issue which broke method modifiers in some cases (thanks @nekoya)
    
     0.52 Mon Jul 13 12:20:03 2009
         - fix t/06_subroutine.t for bleadperl

--- lib/Data/Util.pm
+++ lib/Data/Util.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 #use warnings;
 
-our $VERSION = '0.53';
+our $VERSION = '0.54';
 
 use Exporter;
 our @ISA = qw(Exporter);
@@ -79,7 +79,7 @@ Data::Util - A selection of utilities for data and data types
 
 =head1 VERSION
 
-This document describes Data::Util version 0.53
+This document describes Data::Util version 0.54
 
 =head1 SYNOPSIS
 
