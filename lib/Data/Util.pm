420d134b79e1fcc5ada1cacf7d079fe307171291
gfx <gfuji@cpan.org>
2010-09-13 19:41:58 +0900

    Checking in changes prior to tagging of version 0.58.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 146d0d7..44f7458 100755
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Data-Util
    
    +0.58 Mon Sep 13 19:40:34 2010
    +    - Use GvCV_set() for newer perls
    +
     0.57 Sun Aug  1 17:33:44 2010
         - Fix tests for newer perls

--- lib/Data/Util.pm
+++ lib/Data/Util.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 #use warnings;
 
-our $VERSION = '0.57';
+our $VERSION = '0.58';
 
 use Exporter;
 our @ISA = qw(Exporter);
@@ -79,7 +79,7 @@ Data::Util - A selection of utilities for data and data types
 
 =head1 VERSION
 
-This document describes Data::Util version 0.57
+This document describes Data::Util version 0.58
 
 =head1 SYNOPSIS
 
