0ccb26a8b6a4398253eb4b3988b5de47307da1d4
Fuji Goro <fuji.goro@dena.jp>
2011-10-20 10:56:47 -0700

    Checking in changes prior to tagging of version 0.59.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 44f7458..e1c5df1 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Data-Util
    
    +0.59 2011-10-19 20:08:49
    +    - Re-packaging with better dependencies and latest M::I
    +
     0.58 Mon Sep 13 19:40:34 2010
         - Use GvCV_set() for newer perls

--- lib/Data/Util.pm
+++ lib/Data/Util.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 #use warnings;
 
-our $VERSION = '0.58';
+our $VERSION = '0.59';
 
 use Exporter;
 our @ISA = qw(Exporter);
@@ -79,7 +79,7 @@ Data::Util - A selection of utilities for data and data types
 
 =head1 VERSION
 
-This document describes Data::Util version 0.58
+This document describes Data::Util version 0.59
 
 =head1 SYNOPSIS
 
