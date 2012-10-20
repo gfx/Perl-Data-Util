1b2e13d02c00e475abf5ffcf9d37caeae3dbe433
Fuji, Goro <g.psy.va@gmail.com>
2012-10-20 20:56:52 +0900

    Checking in changes prior to tagging of version 0.60.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e1c5df1..2f7f3d1 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Data-Util
    
    +0.60 2012-10-20 20:54:58
    +    - No feature changes. Just upgraded Module::Install.
    +
     0.59 2011-10-19 20:08:49
         - Re-packaging with better dependencies and latest M::I

--- lib/Data/Util.pm
+++ lib/Data/Util.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 #use warnings;
 
-our $VERSION = '0.59';
+our $VERSION = '0.60';
 
 use Exporter;
 our @ISA = qw(Exporter);
@@ -79,7 +79,7 @@ Data::Util - A selection of utilities for data and data types
 
 =head1 VERSION
 
-This document describes Data::Util version 0.59
+This document describes Data::Util version 0.60
 
 =head1 SYNOPSIS
 
